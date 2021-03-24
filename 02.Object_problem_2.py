8d063cfff25ef8889fbafb2ba20d8629c100a1d9
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-03-24 23:41:53 +0100

    Add solution from udemy

--- 02.Object_problem_2.py
+++ 02.Object_problem_2.py
@@ -40,3 +40,19 @@ class Cylinder:
 c = Cylinder(2,3)
 print(c.volume())
 print(c.surface_area())
+
+
+#           SOLUTION FROM UDEMY:
+
+class Cylinder_2:
+    
+    def __init__(self,height=1,radius=1):
+        self.height = height
+        self.radius = radius
+        
+    def volume(self):
+        return self.height*3.14*(self.radius)**2
+    
+    def surface_area(self):
+        top = 3.14 * (self.radius)**2
+        return (2*top) + (2*3.14*self.radius*self.height)
