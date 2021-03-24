7d070e8cacc8dd588f9d9aa9ff26ec1e57a204fa
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-03-24 23:17:08 +0100

    other solution for exercise

--- 01.Object_problem_1.py
+++ 01.Object_problem_1.py
@@ -49,3 +49,29 @@ li = Line(coordinate1,coordinate2)
 print(li)
 print(li.distance())
 print(li.slope())
+
+
+# my other solution for exercise "classes"
+
+class Line_2:
+    
+    def __init__(self,coor1,coor2):
+        self.x1 = coor1[0]
+        self.x2 = coor2[0]
+        self.y1 = coor1[1]
+        self.y2 = coor2[1]
+    
+    def distance(self):
+        return ((self.x2 - self.x1)**2 + (self.y2 - self.y1)**2)**0.5
+
+    def slope(self):
+        return (self.y2 - self.y1) / (self.x2 - self.x1) 
+
+# TEST
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line_2(coordinate1,coordinate2)
+print(li)
+print(li.distance())
+print(li.slope())
