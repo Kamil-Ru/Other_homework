1e7bd096d0f12036af20de343dc958e46905bcbc
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-14 00:22:48 +0200

    Exercises

---
+++ 100 Python Exercises/015.py
@@ -0,0 +1,10 @@
+''' Exercise 15 - Simple Dictionary
+
+Question: Create a dictionary that contains the keys a  and b  and their respective values 1  and 2 . '''
+
+#my answer
+my_dict = {'a' : 1, 'b' : 2}
+
+
+#Answer 2:
+d = dict(a = 1, b = 2)
---
+++ 100 Python Exercises/017.py
@@ -0,0 +1,10 @@
+''' Exercise 17 - Dictionary Items Sum Up
+
+Question: Calculate the sum of the values of keys a  and b .
+'''
+
+d = {"a": 1, "b": 2, "c": 3}
+
+
+print(d["a"] + d["b"])
+
