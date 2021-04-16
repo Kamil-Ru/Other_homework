f332cf8cae9e94bc6ee834867ba96128739f3f35
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-16 22:48:07 +0200

    Exercises

---
+++ 100 Python Exercises/022.py
@@ -0,0 +1,10 @@
+'''
+Exercise 21 - Dictionary Filtering
+
+Create a dictionary of keys a, b, c where key has a value a list from:
+ - 1 to 10  (for a)
+ - 11 to 20 (for b)
+ - 21 to 30 (for c)
+''' 
+d = {'a':list(range(1,10)),'b':list(range(11,20)),'c':list(range(21,30))}
+print(d)
