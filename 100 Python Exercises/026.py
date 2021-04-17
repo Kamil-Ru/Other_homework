47d0080aad1b93f64f9433a0e29813ab7eaf8049
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-17 23:00:56 +0200

    Test

---
+++ 100 Python Exercises/026.py
@@ -0,0 +1,24 @@
+'''
+
+Exercise 26 - Print Range
+
+Question: Make a script that prints out numbers from 1 to 10
+
+
+Expected output:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+'''
+
+for i in range(0,10):
+    print(i)
