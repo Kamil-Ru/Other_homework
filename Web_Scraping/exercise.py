f0551e93748565d2e9fe5082593b7b6b73095cce
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-17 19:06:28 +0200

    Completed web scraping

--- Web_Scraping/exercise.py
+++ Web_Scraping/exercise.py
@@ -11,19 +11,16 @@ print(result.text)
 # TASK: Get the names of all the authors on the first page.
 
 soup = bs4.BeautifulSoup(result.text,"lxml")
-soup.select('.author')
-authors_import = soup.select('.author')
-authors = []
-for item in authors_import:
-    authors.append(item.text)
+authors = set()
+for item in soup.select('.author'):
+    authors.add(item.text)
     
-set(authors)
 print(*authors, sep = "\n")
 
 #TASK: Create a list of all the quotes on the first page.
-text = soup.select(".text")
+
 quotes = []
-for items in text:
+for items in soup.select(".text"):
     quotes.append(items.text)
 print(*quotes, sep = "\n")
 
@@ -52,22 +49,51 @@ for n in range(1,10):
         authors.append(item.text)
     
 print(set(authors))
+import pprint
+authors = set(authors)
+pprint.pprint(authors)
 
-# 2
-'''
-base_url = 'http://quotes.toscrape.com/page/{}/'
 
-authors = []
-n = 1
+
+
+
+
+# 2 Answer from UDEMY
+# Choose some huge page number we know doesn't exist
+
+url = 'http://quotes.toscrape.com/page/'
+page_url = url+str(9999999)
+
+# Obtain Request
+res = requests.get(page_url)
+
+# Turn into Soup
+soup = bs4.BeautifulSoup(res.text,'lxml')
+
+authors = set()
+page = 1
+
 while True:
-    try:
-        res = requests.get(base_url.format(n))
-        soup = bs4.BeautifulSoup(res.text,"lxml")
-        authors_import = soup.select('.author')
-        for item in authors_import:
-            authors.append(item.text)
-        n += 1
-    if:
+
+    # Concatenate to get new page URL
+    page_url = url+str(page)
+    
+    # Obtain Request
+    res = requests.get(page_url)
+    
+    # Check to see if we're on the last page
+    if "No quotes found!" in res.text:
         break
-set(authors)
-'''
+    
+    # Turn into Soup
+    soup = bs4.BeautifulSoup(res.text,'lxml')
+    
+    # Add Authors to our set
+    for name in soup.select(".author"):
+        authors.add(name.text)
+        
+    # Go to Next Page
+    page += 1
+    
+    
+pprint.pprint(authors)
