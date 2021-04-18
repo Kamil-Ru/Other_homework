1b56cea8cc79f47fc63ff75763400c0bef985009
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-04-18 22:03:40 +0200

    Exercises

---
+++ 100 Python Exercises/032.py
@@ -0,0 +1,14 @@
+'''
+Exercise 32 - Global Variables
+
+Question:  What will the following script output? Please try to do this by mind if you can.
+'''
+
+c = 1
+def foo():
+    return c
+c = 3
+# 3
+print(foo())
+
+
